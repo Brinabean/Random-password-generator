9b331ec3c8db63624dbf077c32f6288cd285c36f
Brina <sabrinazanin97@gmail.com>
2021-12-11 20:29:42 -0500

    added password length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,28 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var passwordlength = function() {
+  var plength = "";
+
+  while (plength === "" || plength === null || plength < 8 || plength > 128) {
+    plength = prompt("How long would you like your password? Please type in any number from 8 to 128.");
+  }
+
+  plength = parseInt(plength);
+
+  console.log("The password length is " + plength);
+  return plength;
+};
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  passwordlength();
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
